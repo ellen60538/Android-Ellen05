12ed4e86bbb2b1e962eb92fef7c0f5cb5e7cf414
user <ellen60538@gmail.com>
2020-03-08 13:09:17 +0800

    2020-03-08-12:00

---
+++ app/src/main/java/tw/org/iii/ellen/ellen05/MainActivity.java
@@ -0,0 +1,122 @@
+package tw.org.iii.ellen.ellen05;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean isRunning ;
+    private Button leftBtn,rightBtn ;
+    private Timer timer = new Timer() ;
+    private int hs ;
+    private Counter counter ;
+    private UIHandler uiHandler = new UIHandler() ;
+    private ListView listView ;
+    TextView clock ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        leftBtn = findViewById(R.id.leftBtn) ;
+        rightBtn = findViewById(R.id.rightBtn) ;
+        clock = findViewById(R.id.clock) ;
+        listView = findViewById(R.id.listView) ;
+
+        changeDisplay() ;
+        clock.setText(parseHS(hs));
+    }
+
+    private void initLap(){
+
+    }
+
+    private void changeDisplay(){
+        leftBtn.setText(isRunning ? "LAP" : "RESET") ;
+        rightBtn.setText(isRunning ? "STOP" : "START") ;
+    }
+
+
+    public void doLeft(View view) {
+        if (isRunning){
+            //LAP
+            doLap() ;
+        }else {
+            //RESET
+            doReset() ;
+        }
+    }
+
+    private void doLap(){
+
+    }
+
+    private void doReset(){
+        hs = 0 ;
+        clock.setText(parseHS(hs));
+    }
+
+    public void doRight(View view) {
+        isRunning = !isRunning ;
+        changeDisplay() ;
+
+        if (isRunning){
+            counter = new Counter() ;
+            timer.schedule(counter,10,10) ;
+        }else {
+            counter.cancel() ;
+            counter = null ;
+        }
+    }
+
+    private class Counter extends TimerTask{
+        @Override
+        public void run() {
+            hs++ ;
+            uiHandler.sendEmptyMessage(0) ;
+        }
+    }
+
+    private class UIHandler extends Handler{
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg) ;
+            clock.setText(parseHS(hs));
+        }
+    }
+
+    private static String parseHS(int hs){
+        int hr, min, sec, phs;
+        phs = hs % 100 ;
+        sec = hs % (100*60) / 100 ;
+        min = hs % (100*60*60) / (100*60) ;
+        hr = hs / (100*60*60) ;
+
+        return  (hr<10 ? "0"+hr : hr) +":"+
+                (min<10 ? "0"+min : min) +":"+
+                (sec<10 ? "0"+sec : sec) +"."+
+                (phs<10 ? "0"+phs : phs);
+    }
+
+    @Override
+    public void finish() {
+        if (timer != null) {
+            timer.cancel() ;
+            timer.purge() ;
+            timer = null ;
+        }
+        super.finish();
+    }
+}
